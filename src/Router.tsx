1671b281a4583f03f141677b7918cf41aa25f1af
Kassidy <kassidyjo97@gmail.com>
2023-03-02 13:44:07 -0600

    initialize repo with react vite and setup react router

---
+++ src/Router.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Navigate,
+  Routes,
+} from 'react-router-dom';
+
+// Pages
+//import { HomePage, AboutPage, PortfolioPage, NotFoundPage } from './pages';
+import HomePage from './pages';
+import AboutPage from './pages/about';
+import ProjectsPage from './pages/projects';
+import NotFoundPage from './pages/404';
+
+class AppRouter extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Routes>
+          // Home
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<Navigate replace to="/" />} />
+          <Route path="/index" element={<Navigate replace to="/" />} />
+          <Route path="/index.html" element={<Navigate replace to="/" />} />
+          // About
+          <Route path="/about" element={<AboutPage />} />
+          // Portfolio
+          <Route path="/projects" element={<ProjectsPage />} />
+          // 404
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+    );
+  }
+}
+
+export default AppRouter;
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppRouter from './Router';
+
+import './scss/index.scss';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <AppRouter />
+  </React.StrictMode>
+);

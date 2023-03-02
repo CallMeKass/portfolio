0da0fe032f2aaf1af1148a100a10de38ea81626b
Kassidy <kassidyjo97@gmail.com>
2023-03-02 17:14:04 -0600

    add logo to favicon and navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import logo from '../../assets/kj-logo.svg';
 
 import './navbar.scss';
 
 export function NavBar() {
   return (
     <div id="navbar" className="navbar">
-      <div className="logo">KJ</div>
+      <a href="/">
+        <img className="logo" src={logo} alt="KJ Logo" />
+      </a>
       <div className="nav-menu">
         <a href="#" className="nav-item">
           Home

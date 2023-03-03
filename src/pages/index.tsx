303e697ffffb89f218aef8377e3a1d689c2834be
Kassidy <kassidyjo97@gmail.com>
2023-03-02 21:18:49 -0600

    change homepage content to jake

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import Logo from '../components/logo';
+import React, { Component, useRef, useState } from 'react';
 import NavBar from '../components/navbar';
 
-function HomePage() {
-  return (
-    <div id="HomePage">
-      <NavBar />
-      <div id="content-container">
-        <h1>Kassidy Jo</h1>
-        <h2>Weow</h2>
+class HomePage extends Component {
+  render() {
+    return (
+      <div id="HomePage">
+        <NavBar />
+        <div id="content-container">
+          <h1>Jakey Jake</h1>
+          <h2>
+            he is so
+            <span className="jake"> wow</span>
+          </h2>
+          <p>also he's my boyfriend and I love him</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default HomePage;

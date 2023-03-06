791a166c05d2494fd80ca580427f65ad15ea2b58
Kassidy <kassidyjo97@gmail.com>
2023-03-05 19:58:09 -0600

    add lorem ipsum to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,19 @@ class HomePage extends Component {
       <div id="HomePage">
         <NavBar />
         <div id="content-container">
-          <h1>Jakey Jake</h1>
-          <h2>
-            he is so
-            <span className="jake"> wow</span>
-          </h2>
-          <p>also he's my boyfriend and I love him</p>
+          <p className="intro-text">Hi, my name is</p>
+          <h1 className="intro-text">Kassidy</h1>
+          <h2>I build things for the web.</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            quis consequat mauris. Sed blandit eros tincidunt mauris aliquam
+            tristique. Duis sagittis egestas mollis. Morbi tincidunt cursus odio
+            vel tincidunt. Curabitur ac magna pretium, maximus arcu non, maximus
+            dui. Mauris hendrerit odio in nisl scelerisque, at posuere nibh
+            pulvinar. Aliquam enim massa, luctus non nisl in, fringilla posuere
+            odio. Phasellus sagittis dapibus nisi, venenatis laoreet risus
+            luctus vel.
+          </p>
         </div>
       </div>
     );
